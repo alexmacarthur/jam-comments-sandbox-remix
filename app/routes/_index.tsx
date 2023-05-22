7baeb9d8f0ba9672f49be2a549b4887ef22213db
Alex MacArthur <alex@macarthur.me>
2023-05-21 22:23:17 -0500

    Set it up.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,37 +1,30 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import { V2_MetaFunction, json } from "@remix-run/node";
+import { posts } from "../data.json";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "New Remix App" }];
 };
 
+export const loader = () => {
+  return json({
+    posts
+  })
+}
+
 export default function Index() {
+  const { posts } = useLoaderData();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+    <div>
+      <h1 className="text-4xl">JamComments Sandbox for Remix</h1>
+
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <a href={`/posts/${post.slug}`}>{post.title}</a>
+          </li>
+        ))}
       </ul>
     </div>
   );
--- app/routes/posts.$slug.tsx
+++ app/routes/posts.$slug.tsx
@@ -1,19 +1,26 @@
 import { posts } from "../data.json";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react"
+import { JamComments, fetchMarkup } from "@jam-comments/remix";
 
-export const loader = ({ params }) => {
+export const loader = async ({ params }) => {
   const post = posts.find((post) => post.slug === params.slug);
 
-  return json({ post });
+  const markup = await fetchMarkup({
+    domain: process.env.JAM_COMMENTS_DOMAIN,
+    apiKey: process.env.JAM_COMMENTS_API_KEY,
+    path: `/posts/${params.slug}`,
+  });
+
+  return json({ post, markup });
 }
 
 export default function Post() {
-  const { post } = useLoaderData();
+  const { post, markup } = useLoaderData();
 
   return (
     <div>
-      <h1>{ post.title }</h1>
+      <h1 className="text-4xl text-center mb-4">{ post.title }</h1>
 
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
@@ -21,6 +28,8 @@ export default function Post() {
         exercitationem voluptatem quas quidem doloribus. Quisquam, quia
         voluptas. Quisquam, quia voluptas.
       </p>
+
+      <JamComments markup={markup} />
     </div>
   )
 }
